9187ccc96d6c5d291cfa5e664fab6ffac4cd29ca
iecastro <41378681+iecastro@users.noreply.github.com>
2018-08-09 10:48:19 -0400

    Add files via upload

--- plot maps.R
+++ plot maps.R
@@ -6,12 +6,14 @@ sf_dots3 <- read_csv("~/Desktop/DataProjects/Census Origins/Data/origin_dots3.cs
 
 sf_dots %>% group_by(Origin) %>% summarise(count = n()) %>% arrange(count)
 
-sf_dots$Origins <- factor(sf_dots$Origin, levels = c("Mexico", "Paraguay", "Uruguay" ,  "Bolivia", "Puerto.Rico","Costa.Rica", "Chile",
-"Panama","Cuba", "Ecuador" , "El.Salvador",  "Guatemala", "Nicaragua", 
-"Honduras","Dominican.Rebublic" ,"Colombia",  "Peru" , "Venezuela", "Argentina"))                  
-                                  
+LA10 <- sf_dots %>% filter(Origin %in% c("Mexico", "Peru", "Puerto.Rico","Ecuador", "Honduras",
+                                         "Colombia","Cuba","El.Salvador", "Guatemala"))
+
+LA10$ordered <- factor(LA10$Origin, levels = c("Mexico", "Peru", "Puerto.Rico","Ecuador", "Honduras",
+                                              "Colombia","Cuba","El.Salvador", "Guatemala"))  
+
 ggplot()  + geom_sf(data = states, fill = NA, color = "#7f7f7f") + 
-  geom_point(data = sf_dots, aes(lon, lat, color = Origins), size = .5) +
+  geom_point(data = LA10, aes(lon, lat, color = ordered), size = .5) +
   theme_minimal() +  theme(axis.text = element_blank(), legend.position = "bottom") +
   scale_color_viridis_d() + guides(colour = guide_legend(override.aes = list(size=3))) +
   ggtitle("US Hispanic/Latino Population by Country of Origin", subtitle = " 1 dot = 50 people") +
@@ -29,11 +31,10 @@ ggplot()  + geom_sf(data = states, fill = NA, color = "#7f7f7f") +
   labs(x = "", y = "",color = "", caption = "Data: US Census - 2016 5-year ACS estimates")
 
 
-
 ggplot()  + geom_sf(data = states, fill = NA, color = "#7f7f7f") + 
   geom_point(data = sf_dots3, aes(lon, lat, color = Race), size = .25) +
   theme_minimal() +  theme(axis.text = element_blank(), legend.position = "bottom") +
-  scale_color_viridis_d() + guides(colour = guide_legend(override.aes = list(size=3))) +
+  scale_color_viridis_d(direction = -1) + guides(colour = guide_legend(override.aes = list(size=3))) +
   ggtitle("US Population by Race", subtitle = " 1 dot = 300 people") +
   labs(x = "", y = "", color = "", caption = "Data: US Census - 2016 5-year ACS estimates")
 
